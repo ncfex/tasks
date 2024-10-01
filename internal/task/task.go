94e92d7f6340f1c1138adf5d3e777d473271cb8f
ncfex <oguzhanguldibi@gmail.com>
2024-10-01 15:47:33 +0300

    Add NewTask function

--- internal/task/task.go
+++ internal/task/task.go
@@ -3,5 +3,16 @@ package task
 import "time"
 
 type Task struct {
-	Opened time.Time
+	ID          int
+	Description string
+	Completed   bool
+	CreatedAt   time.Time
+}
+
+func NewTask(description string) Task {
+	return Task{
+		Description: description,
+		Completed:   false,
+		CreatedAt:   time.Now(),
+	}
 }
